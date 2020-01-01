15c021b6aecb0c8b67060824d931636c18774bdf
Guilherme Sant'Ana <guilherme.sant_ana@outlook.com>
2020-01-01 13:28:00 -0300

    mudando endpoint

--- flask_with_db.py
+++ flask_with_db.py
@@ -1,33 +1,47 @@
 import sqlite3
 from flask import g, Flask, request # g faz parte do application context
+import json
 
 app = Flask(__name__)
 
 DATABASE = './database.db'
 
 
+def make_dicts(cursor, row):
+    return dict((cursor.description[idx][0], value) for idx, value in enumerate(row))
+
+
 def get_db():
     db = getattr(g, '_database',None)
     if db is None:
         db = g._database = sqlite3.connect(DATABASE)
+        db.row_factory = make_dicts
     return db
 
 @app.route('/',methods=['POST'])
 @app.route('/<news_id>/',methods=['GET'])
 def post_or_get(news_id=None):
+    if news_id is not None:
+        print('news_id is not None',news_id)
+        query = 'SELECT rowid, * FROM NEWS WHERE rowid = ?'
+        _id, title, content, tick, site, data = query_db(query,[news_id],one=True)
+        return {'id':_id, 'title': title, 'content': content,'tick': tick, 'site': site, 'data': data}
+    else:
+        return {'ERRO': 'id deve ser expecificado'}
+
+@app.route('/',methods=['POST','GET'])
+def post_or_get_news():
     if request.method == 'POST':
-        print(request.json)
         query = 'INSERT INTO NEWS (title, content, site, data) VALUES(?,?,?,?)'
         tuple_element = (request.json['title'],request.json['content'],request.json['site'],request.json['data'])
         return str(query_db(query,tuple_element))
-    elif request.method == 'GET': 
-        query = 'SELECT rowid, * FROM NEWS WHERE rowid = ?'
-        _id, title, content, tick, site, data = query_db(query,[news_id],one=True)
-        print(query_db(query,[news_id],one=True))
-        return {'id':_id, 'title': title, 'content': content,'tick': tick, 'site': site, 'data': data}
+    elif request.method == 'GET':
+        query ='SELECT rowid, * FROM NEWS'
+        return {'results': query_db(query)}
     else:
+        print('request method was', request.method)
         raise Exception
-
+    
 @app.teardown_appcontext
 def close_connection(exception):
     db = getattr(g,'_database',None)
